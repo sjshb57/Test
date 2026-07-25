6e155c97e5c1a30392fbc0a9a0902ab802ce8ddf
拜託 <98471248+sjshb57@users.noreply.github.com>
2026-07-25 15:05:37 +0800

    Update User-Agent header to Chrome 125.0

--- archive.py
+++ archive.py
@@ -137,7 +137,7 @@ HEADERS = {
     "User-Agent": (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
         "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/124.0.0.0 Safari/537.36"
+        "Chrome/125.0.0.0 Safari/537.36"
     ),
     "Referer": "https://web.archive.org/",
 }
